bde314d7c3466cabaa40a71ba369a7faac51fde8
Meowyam <maryam@chilon.net>
2024-12-13 13:44:54 +0800

    added status bar button you can press

--- src/extension.ts
+++ src/extension.ts
@@ -4,11 +4,26 @@ import { RuleToJson } from './ruleToJson';
 
 export function activate(context: vscode.ExtensionContext) {
   let disposable = vscode.commands.registerCommand('viz.showViz', () => {
-    showViz(context);
+    const editor = vscode.window.activeTextEditor;
+    if (editor) {
+      const rule = editor.document.getText();
+      const json = RuleToJson.parse(rule);
+      showViz(context, json);
+    }
   });
 
   context.subscriptions.push(disposable);
 
+  // on Button. the button is at the bottom right of the status bar.
+
+  const button = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
+  button.command = 'viz.showViz';
+  button.text = 'Update Diagram';
+  button.tooltip = 'Show visualisation';
+  button.show();
+
+
+  // on save
   vscode.workspace.onDidSaveTextDocument((document) => {
     if (document === vscode.window.activeTextEditor?.document) {
       const rule = document.getText();
